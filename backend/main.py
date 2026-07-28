a5c75e0f698721e5b388f0081e4f7417d834932e
Ankithagowdasr <ankithagowda621@gmail.com>
2026-07-28 11:45:32 +0530

    Add resume parsing libraries

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import os
+import shutil
 
-app = FastAPI()
+app = FastAPI(
+    title="AI Resume Analyzer",
+    version="1.0"
+)
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 
 @app.get("/")
 def home():
     return {
         "message": "Welcome to AI Resume Analyzer"
+    }
+
+
+@app.post("/upload")
+async def upload_resume(file: UploadFile = File(...)):
+    # Check file type
+    if not (file.filename.endswith(".pdf") or file.filename.endswith(".docx")):
+        return {
+            "error": "Only PDF and DOCX files are allowed."
+        }
+
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "filename": file.filename,
+        "status": "Resume uploaded successfully"
     }
